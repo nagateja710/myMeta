4442d9cbbe3380a64f5c1950edd9efe01f511a69
nagateja710 <nagatejatejavath@gmail.com>
2025-12-29 13:15:46 +0530

    v7.1.2

--- src/app/friends/page.js
+++ src/app/friends/page.js
@@ -6,9 +6,9 @@ export default function BooksPage() {
 
 
   return (
-    <div className="min-h-screen bg-linear-150 from-green-400  to-white">
-        <div className="py-10 px-10 text-purple-400 text-5xl font-bold">comming soon!
-            <div className="py-2 px-10 text-white text-xs font-light">see what your friends are upto and discover together!</div>
+    <div className="min-h-screen bg-linear-150 from-green-400  to-emerald-700">
+        <div className="py-10 px-10 text-white  text-5xl font-bold">comming soon!
+            <div className="py-2 px-10 text-yellow-200 text-xs font-light">see what your friends are upto and discover together!</div>
         </div>
         
      
